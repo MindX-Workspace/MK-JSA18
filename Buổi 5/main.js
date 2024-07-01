aee600b81e9aceb2399ea790e67c1d26cb298651
whis102 <chicongnguyen.dev@gmail.com>
2024-07-01 20:58:42 +0700

    update

---
+++ Buổi 5/main.js
@@ -0,0 +1,22 @@
+const currentTheme = sessionStorage.getItem("theme");
+
+const darkTheme = document.getElementById("dark-mode");
+const lightTheme = document.getElementById("light-mode");
+
+if (currentTheme) {
+  document.body.classList.add(currentTheme);
+}
+
+darkTheme.addEventListener("click", () => {
+  document.body.classList.remove("light");
+  document.body.classList.add("dark");
+
+  sessionStorage.setItem("theme", "dark");
+});
+
+lightTheme.addEventListener("click", () => {
+  document.body.classList.remove("dark");
+  document.body.classList.add("light");
+
+  sessionStorage.setItem("theme", "light");
+});
