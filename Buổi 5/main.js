af1ea401e3cf746acf1da4be975c03b87340e883
whis102 <chicongnguyen.dev@gmail.com>
2024-07-01 21:00:00 +0700

    local storage

--- Buổi 5/main.js
+++ Buổi 5/main.js
@@ -1,3 +1,4 @@
+// Session Storage
 const currentTheme = sessionStorage.getItem("theme");
 
 const darkTheme = document.getElementById("dark-mode");
@@ -20,3 +21,27 @@ lightTheme.addEventListener("click", () => {
 
   sessionStorage.setItem("theme", "light");
 });
+
+// Local Storage
+// const currentTheme = localStorage.getItem("theme");
+
+// const darkTheme = document.getElementById("dark-mode");
+// const lightTheme = document.getElementById("light-mode");
+
+// if (currentTheme) {
+//   document.body.classList.add(currentTheme);
+// }
+
+// darkTheme.addEventListener("click", () => {
+//   document.body.classList.remove("light");
+//   document.body.classList.add("dark");
+
+//   localStorage.setItem("theme", "dark");
+// });
+
+// lightTheme.addEventListener("click", () => {
+//   document.body.classList.remove("dark");
+//   document.body.classList.add("light");
+
+//   localStorage.setItem("theme", "light");
+// });
